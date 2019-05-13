bac4a5537b0fcc478b139ef5796e681a020a5d93
Ben Gamari <ben@smart-cactus.org>
2019-05-13 11:18:54 -0400

    Changes

--- main.rs
+++ main.rs
@@ -27,9 +27,9 @@ struct OnSphere;
 impl Distribution<V3<f64>> for OnSphere {
     fn sample<R: rand::Rng + ?Sized>(&self, rng: &mut R) -> V3<f64> {
         use std::f64::consts::PI;
-        use rand::distributions::Range;
-        let phi = rng.sample(Range::new(0.0, PI));
-        let theta = rng.sample(Range::new(-PI, PI));
+        use rand::distributions::Uniform;
+        let phi = rng.sample(Uniform::new(0.0, PI));
+        let theta = rng.sample(Uniform::new(-PI, PI));
         V3 {
             x: theta.sin() * phi.cos(),
             y: theta.sin() * phi.sin(),
@@ -46,15 +46,42 @@ fn beam_intensity(beam_size: V3<f64>, p: V3<f64>) -> f64 {
     (-alpha / 2.0).exp()
 }
 
+struct Promise<T>(std::sync::mpsc::Receiver<T>);
+
+fn await<T>(promise: Promise<T>) -> T {
+    let Promise(recv) = promise;
+    recv.recv().unwrap()
+}
+
+fn parallel<T, F>(f: F) -> Promise<T> where
+    T: 'static + Send,
+    F: 'static + Send + Fn () -> T
+{
+    use std::thread;
+    use std::sync::mpsc::channel;
+    let (sender, receiver) = channel();
+    thread::spawn(move || {
+        let x = f();
+        sender.send(x).unwrap();
+    });
+    Promise(receiver)
+}
+
 fn main() {
     use std::vec::Vec;
-    use rand::NewRng;
-    let mut rng = rand::SmallRng::new();
-    let walk = RandomWalk {
-        rng: rng, diffusivity: 1.0, pos: V3::origin()
-    };
-    let steps: Vec<V3<f64>> = walk.take(10000000).collect();
-    let s: V3<f64> = steps.into_iter().sum();
-    println!("hello {:?}", s);
+    use rand::FromEntropy;
+    let beam_size = V3 {x:1.0, y:1.0, z:10.0};
+    let promise = parallel(move || {
+        let rng = rand::rngs::SmallRng::from_entropy();
+        let walk = RandomWalk {
+            rng: rng, diffusivity: 1.0, pos: V3::origin()
+        };
+        let steps: Vec<f64> = walk.map(|x| beam_intensity(beam_size, x)).take(10000000).collect();
+        //let steps: Vec<V3<f64>> = walk.map(|x| beam_intensity(beam_size, x)).take(10000000).collect();
+        let s: f64 = steps.into_iter().sum();
+        println!("hello {:?}", s);
+    });
+
+    await(promise);
     //debug!("Hello {}", steps);
 }
