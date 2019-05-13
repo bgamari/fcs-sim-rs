4793b4ff777fc8e6273c1e61bedd2cd35a81e99c
Ben Gamari <ben@smart-cactus.org>
2019-05-13 19:02:01 -0400

    Freeze nixpkgs

--- default.nix
+++ default.nix
@@ -1,5 +1,12 @@
 { nixpkgs ? (import <nixpkgs> {}) }:
-
+let
+  nixpkgsSrc = fetchGit {
+    url = "https://github.com/nixos/nixpkgs";
+    rev = "7cd2e4ebe8ca91f829b405451586868744270100";
+    ref = "release-19.03";
+  };
+  nixpkgs = import nixpkgsSrc {};
+in
 with nixpkgs;
 callPackage (import ./fcs-sim.nix) {
   inherit (rustPlatform) buildRustPackage;
