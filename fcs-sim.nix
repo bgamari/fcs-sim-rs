bf944dc8e1a644fa893d4d2a0230478d66c1dc17
Ben Gamari <ben@smart-cactus.org>
2019-05-13 18:58:19 -0400

    Update nix dependencies

--- fcs-sim.nix
+++ fcs-sim.nix
@@ -5,5 +5,5 @@ buildRustPackage rec {
   version = "0.1.0";
 
   src = ./.;
-  cargoSha256 = "1syqc2ccx1vf7pcjy9yi9bww8s5111vp7d21nffjsgz9yi5qsrxh";
+  cargoSha256 = "1vb22q6vrrbr6dn299aw4xhldz5ws3mixn4yn34y3m7jx2276x3b";
 }
